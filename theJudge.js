75e1d2685e996fab2619241062d76c508247443d
Spenu12994 <48360079+spenu1@users.noreply.github.com>
2024-08-06 12:40:40 +0100

    The answer reveals itself

---
+++ theJudge.js
@@ -0,0 +1,17 @@
+let answer = "moonlight"
+let randFac = 213;
+let inputField = document.getElementById("theAnswer");
+
+function solveTheProblem(){
+    let inputAnswer = inputField.value;
+    if(inputField == answer){
+        
+    }
+    else{
+        showIncorrect(inputAnswer);
+    }
+}
+
+function showIncorrect(inputAnswer){
+
+}
